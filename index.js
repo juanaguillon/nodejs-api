cfbd79bcae897cf88ec31d0bd3f4a87c84db79f1
Juan Aguillon <miloca.98@hotmail.com>
2018-09-20 09:39:13 -0500

    Delete get Tester

--- index.js
+++ index.js
@@ -10,10 +10,6 @@ app.set('port', process.env.PORT || 3000 );
 app.use( urlEncoder );
 app.use( jsonParser );
 
-app.get('/:name', function( req, res ){
-  res.send({Message:`Hola ${req.params.name}`});
-})
-
 app.listen(app.get('port'),()=>{
   console.log('Servidor funcionando en puerto', app.get('port') );
 })
