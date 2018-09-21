6b51750a3133e07cd3c34921630a1b272a0d10e0
Juan Aguillon <miloca.98@hotmail.com>
2018-09-21 12:07:21 -0500

    Creando nueva ruta
    Se crea una ruda con autenticación de middleware.

--- controllers/routes.js
+++ controllers/routes.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const urlEncoder = bodyParser.urlencoded({ extended: false });
 const jsonParser = bodyParser.json();
 const productControll = require('./product-controllers');
+const auth = require('../middle/auth.middleware');
 
 app.use(urlEncoder);
 app.use(jsonParser);
@@ -15,6 +16,9 @@ app.get('/api/product/:product_id', productControll.getProduct)
 app.post('/api/product/', productControll.addProduct );
 app.put('/api/product/:product_id', productControll.updateProduct)
 app.delete('/api/product/:product_id', productControll.deleteProduct);
+app.get('/private', auth, ( ) => {
+  res.status(200).send({message:'Tienes acceso'});
+} ); 
 
 app.set('port', port );
 
